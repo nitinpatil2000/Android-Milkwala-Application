3a1ed4f5fc1b0c86a46a49e76cc31446abc4db49
chetan <chetan@technosoul.io>
2023-04-30 22:57:12 +0530

    Updated SplashScreen flow to decide which screen should be displayed after SplashScreen.

--- app/src/main/java/com/technosoul/milkwala/Constants.java
+++ app/src/main/java/com/technosoul/milkwala/Constants.java
@@ -2,5 +2,9 @@ package com.technosoul.milkwala;
 
 public class Constants {
 
+    private static final String PKG = BuildConfig.APPLICATION_ID;
     public static final String SHARED_PREF_NAME = "MilkwalaSharedPreference";
+    public static final String KEY_LOGIN_TYPE = PKG + ".login_type";
+    public static final int LOGIN_TYPE_ADMIN = 0;
+    public static final int LOGIN_TYPE_DELIVERY_BOY = 1;
 }
--- app/src/main/java/com/technosoul/milkwala/ui/SplashActivity.java
+++ app/src/main/java/com/technosoul/milkwala/ui/SplashActivity.java
@@ -3,14 +3,16 @@ package com.technosoul.milkwala.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 
+import com.technosoul.milkwala.Constants;
 import com.technosoul.milkwala.MainActivity;
 import com.technosoul.milkwala.R;
 import com.technosoul.milkwala.accounts.auth.AuthTokenManager;
 import com.technosoul.milkwala.auth.AuthActivity;
+import com.technosoul.milkwala.managers.SharedPreferenceManager;
 import com.technosoul.milkwala.todaydeliver.DeliveryActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -24,21 +26,21 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 String token = AuthTokenManager.instance().get();
-                SharedPreferences preferences= getSharedPreferences("login", MODE_PRIVATE);
-                Boolean check= preferences.getBoolean("flag", false);
-                Boolean deliver = preferences.getBoolean("deliver", false);
-                Intent iDashBoard;
-                if (check) {
-                    iDashBoard =new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent;
+                if (TextUtils.isEmpty(token)) {
+                    // Don't have a token. Let's show Login Screen.
+                    intent = new Intent(SplashActivity.this, AuthActivity.class);
+                } else {
+                    // User is already logged in. Let's check the login type
+                    int loginType = SharedPreferenceManager.getInstance().getInt(Constants.KEY_LOGIN_TYPE, Constants.LOGIN_TYPE_ADMIN);
+                    if (loginType == Constants.LOGIN_TYPE_DELIVERY_BOY) {
+                        intent = new Intent(SplashActivity.this, DeliveryActivity.class);
+                    } else {
+                        intent = new Intent(SplashActivity.this, MainActivity.class);
+                    }
+                }
 
-                }else if(deliver){
-                    iDashBoard = new Intent(SplashActivity.this, DeliveryActivity.class );
-                }
-                else{
-                    iDashBoard = new Intent(SplashActivity.this, AuthActivity.class);
-                }
-                startActivity(iDashBoard);
-//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(intent);
             }
         }, 4000);
 
